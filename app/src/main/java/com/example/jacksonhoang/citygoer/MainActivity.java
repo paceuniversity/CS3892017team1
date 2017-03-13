cd0dbe91ee04047274492207629e989b6a5140dd
jackson hoang <jacksonhoang@hotmail.com>
2017-03-12 20:55:43 -0400

    4th Commit

---
+++ app/src/main/java/com/example/jacksonhoang/citygoer/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.jacksonhoang.citygoer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void phraseSelection(View view) {
+        Intent startPhrase = new Intent(this, phrasesActivity.class);
+        startActivity(startPhrase);
+    }
+
+    public void foodSelection(View view) {
+        Intent startFood = new Intent(this, foodActivity.class);
+        startActivity(startFood);
+    }
+
+    public void attractionSelection(View view) {
+        Intent startAttraction = new Intent(this, attractionActivity.class);
+        startActivity(startAttraction);
+    }
+
+
+}
