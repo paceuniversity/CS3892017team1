e47097d5d0055b15e9a49f8c08fb696ece487e03
jackson hoang <jacksonhoang@hotmail.com>
2017-03-19 17:23:41 -0400

    7th Commit:  Facts activity with scroll bar and 15 facts

--- app/src/main/java/com/example/jacksonhoang/citygoer/factsActivity.java
+++ app/src/main/java/com/example/jacksonhoang/citygoer/factsActivity.java
@@ -3,7 +3,9 @@ package com.example.jacksonhoang.citygoer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class factsActivity extends AppCompatActivity {
 
@@ -11,6 +13,8 @@ public class factsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facts);
+        TextView fact1 = (TextView) findViewById(R.id.fact1);
+        fact1.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void mainSelection(View view) {
