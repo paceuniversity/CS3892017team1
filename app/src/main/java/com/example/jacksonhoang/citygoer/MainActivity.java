47c02bc8bb6ef18ee353aebefeb69baecec2e238
Hana Stanojkovic <hanastanojkovic@gmail.com>
2017-04-02 14:51:51 -0400

    added custom toolbar. Will continue designing it.

--- app/src/main/java/com/example/jacksonhoang/citygoer/MainActivity.java
+++ app/src/main/java/com/example/jacksonhoang/citygoer/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
     public void phraseSelection(View view) {
@@ -32,6 +35,4 @@ public class MainActivity extends AppCompatActivity {
         Intent startAttraction = new Intent(this, factsActivity.class);
         startActivity(startAttraction);
     }
-
-
 }
