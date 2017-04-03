efac43c2a8fbf9fb05444dddb31865322e311225
Hana Stanojkovic <hanastanojkovic@gmail.com>
2017-04-03 14:38:25 -0400

    added back buttons on the toolbar in the new attraction screens

--- app/src/main/java/com/example/jacksonhoang/citygoer/attractionMuseumActivity.java
+++ app/src/main/java/com/example/jacksonhoang/citygoer/attractionMuseumActivity.java
@@ -2,6 +2,8 @@ package com.example.jacksonhoang.citygoer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class attractionMuseumActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class attractionMuseumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attraction_museum);
+        // toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
