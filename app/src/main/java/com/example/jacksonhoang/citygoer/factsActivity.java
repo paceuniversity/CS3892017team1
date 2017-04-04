b5ea7bfc3c61af1777def94fc34700bc80e00967
Eiman Ahmed <eimanahmed96@gmail.com>
2017-04-04 16:38:16 -0400

    Updated UI for facts, also refresh button to get new facts every time

--- app/src/main/java/com/example/jacksonhoang/citygoer/factsActivity.java
+++ app/src/main/java/com/example/jacksonhoang/citygoer/factsActivity.java
@@ -1,13 +1,14 @@
 package com.example.jacksonhoang.citygoer;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class factsActivity extends AppCompatActivity {
 
@@ -27,8 +28,37 @@ public class factsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-        TextView fact1 = (TextView) findViewById(R.id.fact1);
-        fact1.setMovementMethod(new ScrollingMovementMethod());
+        final ArrayList<String> facts =new ArrayList<String>();
+        facts.add("Beijing is the capital of the People's republic of China");
+        facts.add("Bei means northern and jin means capital. Thus, Beijing literally means the north capital");
+        facts.add("It is home to the world's longest wall");
+        facts.add("Beijing's Forbidden City is the largest palace in the world");
+        facts.add("Bejing was the capital for more than 6 dynsaties and governments");
+        facts.add("Has 13 suburban districts: Changping, Daxing, Dongcheng, Fangshan, Fengtai Huairou, Mentougou, Miyun, Pinggu, hijingshan, Shunyi, Tongzhou, Yanqing");
+        facts.add("Two main airports: Capital International and Nanyuan Airport");
+        facts.add("The Forbidden City has a Palace Museum with imperial collections of Chinese art");
+        facts.add("Beijing City was established over 3,000 years ago");
+        facts.add("Beijing hosts one of the earliest living humans called Peking Man");
+        facts.add("On October 1st 1949, the People's Republic of China was founded");
+        facts.add("Forbidden City consists of 980 buildings taking 7,800,000 square feet");
+
+        final int info = facts.size()-1;
+        int numb =  (int)(Math.random()*info);
+        String factOutput= facts.get(numb);
+        TextView fact1 = (TextView) findViewById(R.id.facts_text);
+        fact1.setText(factOutput.toString());
+
+        Button button = (Button) findViewById(R.id.refreshbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                int numb =  (int)(Math.random()*info);
+                String factOutput= facts.get(numb);
+                TextView fact1 = (TextView) findViewById(R.id.facts_text);
+                fact1.setText(factOutput.toString());
+
+            }
+        });
     }
 
     @Override
@@ -39,4 +69,6 @@ public class factsActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
