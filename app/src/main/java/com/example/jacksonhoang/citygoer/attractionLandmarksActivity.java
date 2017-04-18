03941f521e9f88c9049864d89bf62d6ce395136e
jackson hoang <jacksonhoang@hotmail.com>
2017-04-18 16:22:09 -0400

    Attraction Commit: Fragments for all Landmark attractions with google map API implementation.

--- app/src/main/java/com/example/jacksonhoang/citygoer/attractionLandmarksActivity.java
+++ app/src/main/java/com/example/jacksonhoang/citygoer/attractionLandmarksActivity.java
@@ -1,11 +1,84 @@
 package com.example.jacksonhoang.citygoer;
 
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import layout.ForbiddenCity;
+import layout.MingTombs;
+import layout.SummerPalace;
+import layout.TempleOfHeaven;
+import layout.greatwall_fragment;
+
 
 public class attractionLandmarksActivity extends AppCompatActivity {
+
+    Button b1;
+
+    public void init() {
+        b1 = (Button) findViewById(R.id.greatWall);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new greatwall_fragment();
+                FragmentTransaction map = getSupportFragmentManager().beginTransaction();
+                map.replace(R.id.activity_main, fragment).commit();
+            }
+        });
+    }
+    public void init2() {
+        b1 = (Button) findViewById(R.id.forbiddenCity);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new ForbiddenCity();
+                FragmentTransaction map = getSupportFragmentManager().beginTransaction();
+                map.replace(R.id.activity_main, fragment).commit();
+            }
+        });
+    }
+    public void init3() {
+        b1 = (Button) findViewById(R.id.templeOfHeaven);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new TempleOfHeaven();
+                FragmentTransaction map = getSupportFragmentManager().beginTransaction();
+                map.replace(R.id.activity_main, fragment).commit();
+            }
+        });
+    }
+    public void init4() {
+        b1 = (Button) findViewById(R.id.mingTombs);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new MingTombs();
+                FragmentTransaction map = getSupportFragmentManager().beginTransaction();
+                map.replace(R.id.activity_main, fragment).commit();
+            }
+        });
+    }
+    public void init5() {
+        b1 = (Button) findViewById(R.id.summerPalace);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new SummerPalace();
+                FragmentTransaction map = getSupportFragmentManager().beginTransaction();
+                map.replace(R.id.activity_main, fragment).commit();
+            }
+        });
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +95,11 @@ public class attractionLandmarksActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+        init();
+        init2();
+        init3();
+        init4();
+        init5();
     }
 
     @Override
@@ -32,4 +110,6 @@ public class attractionLandmarksActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
