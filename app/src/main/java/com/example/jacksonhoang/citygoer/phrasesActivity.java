388e5dff6145bc453b562afd6ce7decd2ce3148a
Hana Stanojkovic <hanastanojkovic@gmail.com>
2017-04-23 22:20:40 -0400

    Created a Translate activity and also figured out how to push only the necessary files

--- app/src/main/java/com/example/jacksonhoang/citygoer/phrasesActivity.java
+++ app/src/main/java/com/example/jacksonhoang/citygoer/phrasesActivity.java
@@ -1,9 +1,11 @@
 package com.example.jacksonhoang.citygoer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 public class phrasesActivity extends AppCompatActivity {
 
@@ -24,6 +26,10 @@ public class phrasesActivity extends AppCompatActivity {
         }
     }
 
+    public void translationSelection(View view) {
+        Intent startTranslate = new Intent(this, translateActivity.class);
+        startActivity(startTranslate);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
