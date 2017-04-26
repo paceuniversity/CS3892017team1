3e5511c941d195afc43908fb6a5153e7d407dd7c
Hana Stanojkovic <hanastanojkovic@gmail.com>
2017-04-26 14:21:58 -0400

    Some design changes primarily in Translate feature

--- app/src/main/java/com/example/jacksonhoang/citygoer/translateActivity.java
+++ app/src/main/java/com/example/jacksonhoang/citygoer/translateActivity.java
@@ -1,8 +1,12 @@
 package com.example.jacksonhoang.citygoer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
 
 public class translateActivity extends AppCompatActivity {
 
@@ -15,6 +19,7 @@ public class translateActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
 
         // add back arrow to toolbar
         if (getSupportActionBar() != null){
