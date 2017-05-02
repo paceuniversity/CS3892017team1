dc212fa75c1e736189b53af15354e07812353230
Hana Stanojkovic <hanastanojkovic@gmail.com>
2017-05-02 15:22:08 -0400

    Added functionality to the Translate feature

--- app/src/main/java/com/example/jacksonhoang/citygoer/phrasesActivity.java
+++ app/src/main/java/com/example/jacksonhoang/citygoer/phrasesActivity.java
@@ -1,14 +1,18 @@
 package com.example.jacksonhoang.citygoer;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
 
 
 public class phrasesActivity extends AppCompatActivity {
+
+    Button translateButton;
+    TextView translatedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,23 @@ public class phrasesActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
+
+        translateButton = (Button)findViewById(R.id.translateButton);
+        translatedText =(TextView)findViewById(R.id.translatedText);
+        translateButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                translatedText.setText(getMessageToTranslate());
+            }
+        });
     }
+
+    public String getMessageToTranslate() {
+        EditText messageToTranslate = (EditText) findViewById(R.id.messageToTranslate);
+        String textToTranslate = messageToTranslate.getText().toString();
+        return textToTranslate;
+    }
+
 
 }
