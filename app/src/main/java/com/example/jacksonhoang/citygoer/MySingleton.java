f8e82537c84cac148f577794d2299ea671b26bc2
Hana Stanojkovic <hanastanojkovic@gmail.com>
2017-05-09 00:10:22 -0400

    Got the translation feature functionality working

---
+++ app/src/main/java/com/example/jacksonhoang/citygoer/MySingleton.java
@@ -0,0 +1,62 @@
+package com.example.jacksonhoang.citygoer; /**
+ * Created by Hana on 5/8/17.
+ */
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import android.graphics.Bitmap;
+import android.content.Context;
+import android.util.LruCache;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+
+public class MySingleton {
+    private static MySingleton mInstance;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private static Context mCtx;
+
+    private MySingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized MySingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new MySingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+}
--- app/src/main/java/com/example/jacksonhoang/citygoer/phrasesActivity.java
+++ app/src/main/java/com/example/jacksonhoang/citygoer/phrasesActivity.java
@@ -5,8 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 
 public class phrasesActivity extends AppCompatActivity {
@@ -38,16 +49,65 @@ public class phrasesActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                translatedText.setText(getMessageToTranslate());
+//                translatedText.setText(getMessageToTranslate());
+                EditText messageToTranslate = (EditText) findViewById(R.id.messageToTranslate);
+                String textToTranslate = messageToTranslate.getText().toString();
+                sendRequest(textToTranslate);
             }
         });
     }
 
-    public String getMessageToTranslate() {
-        EditText messageToTranslate = (EditText) findViewById(R.id.messageToTranslate);
-        String textToTranslate = messageToTranslate.getText().toString();
-        return textToTranslate;
+    public void sendRequest(String input){
+        final TextView mTxtDisplay;
+        ImageView mImageView;
+        mTxtDisplay = (TextView) findViewById(R.id.translatedText); //xml things
+//        This works! now gonna test something with the URL...
+//        String url = "https://translation.googleapis.com/language/translate/v2?target=en&q=bonjour&key=AIzaSyCjTLl2fQYYfd4n1n2QN5smy0Cu85-7ySI";
+        String url = "https://translation.googleapis.com/language/translate/v2?";
+        String target = "target=zh-CN";
+//        Make sure the input replaces spaces with %20!
+        String q = "&q="+input;
+        String key = "&key=AIzaSyCjTLl2fQYYfd4n1n2QN5smy0Cu85-7ySI";
+        url = url.concat(target).concat(q).concat(key);
+        url = url.replaceAll(" ", "%20");
+
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, (String)null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+//                        Call a method to parse the response
+                        String parsedResponse="";
+                        try {
+                            parsedResponse = parseResponse(response);
+                            mTxtDisplay.setText(parsedResponse);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            mTxtDisplay.setText("Something went wrong");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        mTxtDisplay.setText("Didn't work :(");
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
     }
+
+    public String parseResponse(JSONObject response) throws JSONException {
+        JSONObject mainObject = response.getJSONObject("data");
+        JSONObject obj = mainObject.getJSONArray("translations").getJSONObject(0);
+        String translatedText = obj.getString("translatedText");
+
+        return translatedText;
+    }
+
 
 
 }
